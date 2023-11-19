e52cd796bfd97883dca6858adeafa39775831826
Nicholas Sciarretta <39418699+nscrrtta@users.noreply.github.com>
2023-11-18 18:16:20 -0800

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+from constants import png_path, opposite_faces
 from solve_scramble import solve, scramble
 from button import Button
 from cube import Cube
 from face import Face
 from box import Box
-# import subprocess
+import subprocess
 import threading
 import constants
-# import motors
+import motors
 import pygame
 import os
 
@@ -26,9 +27,9 @@ cube = Cube()
 faces = [Face(i) for i in range(6)]
 boxes = [Box (i) for i in range(6)]
 
-FRU_png = pygame.image.load('png/FRU.png')
-LBD_png = pygame.image.load('png/LBD.png')
-red_x   = pygame.image.load('png/red_x.png')
+FRU_png = pygame.image.load(f'{png_path}/FRU.png')
+LBD_png = pygame.image.load(f'{png_path}/LBD.png')
+red_x   = pygame.image.load(f'{png_path}/red_x.png')
 
 solve_btn    = Button(482, 170, 'solve')
 scramble_btn = Button(482, 257, 'scramble')
@@ -59,14 +60,14 @@ while running:
             elif solve_btn.clicked((x,y)) and solveable:
                 selected_colour = -1
                 for box in boxes: box.selected = False
-                # motors.GPIO.output(motors.enable_pin, 0)
+                motors.GPIO.output(motors.enable_pin, 0)
                 threading.Thread(target=solve, args=(cube,), daemon=True).start()
 
             # Scramble
             elif scramble_btn.clicked((x,y)) and solveable:
                 selected_colour = -1
                 for box in boxes: box.selected = False
-                # motors.GPIO.output(motors.enable_pin, 0)
+                motors.GPIO.output(motors.enable_pin, 0)
                 threading.Thread(target=scramble, args=(cube,), daemon=True).start()
 
             # Select a sticker
@@ -101,17 +102,17 @@ while running:
         curr_move = cube.moves.pop(0)
         move_set = [curr_move]
 
-        if cube.moves and cube.moves[0][0] == constants.opposite_faces[curr_move[0]]:
+        if cube.moves and cube.moves[0][0] == opposite_faces[curr_move[0]]:
             move_set.append(cube.moves.pop(0)) # Next move can be made at the same time
 
-        # threads:list[threading.Thread] = []
-        # for move in move_set:
-        #     m, n = move[0], int(move[1])
-        #     motor = motors.motor_dict[m]
-        #     threads.append(threading.Thread(target=motor.turn, args=(n,), daemon=True))
+        threads:list[threading.Thread] = []
+        for move in move_set:
+            m, n = move[0], int(move[1])
+            motor = motors.motor_dict[m]
+            threads.append(threading.Thread(target=motor.turn, args=(n,), daemon=True))
 
-        # for thread in threads: thread.start()
-        # for thread in threads: thread.join()
+        for thread in threads: thread.start()
+        for thread in threads: thread.join()
 
         for move in move_set: # Make move(s) in code to update GUI
             cube.make_move(move, save_move=False)
@@ -124,8 +125,8 @@ while running:
         for btn in [solve_btn, scramble_btn, cancel_btn]:
             if btn.pressed: btn.pressed = False
 
-        # if not motors.GPIO.input(motors.enable_pin):
-        #     motors.GPIO.output(motors.enable_pin, 1)
+        if not motors.GPIO.input(motors.enable_pin):
+            motors.GPIO.output(motors.enable_pin, 1)
 
 
     # Draw GUI
